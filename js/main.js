9523a5c36c6aa88da595102b586d0317caed54e7
Isaac White <iaw2105@columbia.edu>
2013-11-28 17:05:19 -0500

    Generalizing search to take either string or just location based. Also first draft of comment and venue objects.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,171 @@
-W4170 = {};
-W4170.oauth = "EJJLH0BUCOHLVG2NVX1Z0ZK1IKWYWWXJSQUVR2VBG4IYG00G"
+TRP = {};
+TRP.oauth = "EJJLH0BUCOHLVG2NVX1Z0ZK1IKWYWWXJSQUVR2VBG4IYG00G";
+TRP.fsAPI = "https://api.foursquare.com/v2";
+TRP.currLoc = {lat: 40.7, lon: -74};
 
-W4170.getRecommendedByLocation = function(lat,lon,callback) {
-	var fsAPI = "https://api.foursquare.com/v2";
-
-	function getResults(lat,lon) {
-		var endpoint = fsAPI + "/venues/explore";
-		var dateNow = new Date();
-		var dateString = "" + 
-			dateNow.getFullYear() + 
-			(dateNow.getMonth() + 1) +
-			dateNow.getDate();
-		var requestUrl = endpoint + "?";
-		requestUrl    += "ll=" + lat + "," + lon;
-		requestUrl    += "&oauth_token=" + W4170.oauth;
-		requestUrl    += "&v=" + dateString;
-
-		$.get(requestUrl,callback);
-	}
-
-	getResults(lat,lon);
+TRP.getDateString = function() {
+    var dateNow = new Date();
+    var dateString = "" + 
+        dateNow.getFullYear() + 
+        (dateNow.getMonth() + 1) +
+        dateNow.getDate();
+    return dateString;
+}
+TRP.Venue = function(prop) {
+    this.name = prop.name;
+    this.rating = prop.rating;
+    this.coord = prop.locInfo.coord;
+    this.address = prop.locInfo.address;
+    this.thumb = prop.thumbnail;
+    this.comments = prop.comments;
+    this.url = prop.url;
 }
 
-W4170.lat = 40.7;
-W4170.lon = -74;
+TRP.Comment = function(prop) {
+    this.text     = prop.text;
+    this.uFirst   = prop.firstName;
+    this.uLast    = prop.lastName;
+    this.photoUrl = prop.photoUrl;
+}
 
-W4170.getRecommendedByLocation(W4170.lat,W4170.lon,function(data) {
-	console.log(data);
-})
+TRP.getSuggestions = function(param) {
+
+    var lat = param.lat;
+    var lon = param.lon;
+    var callback = param.callback;
+    var search = param.search;
+
+    //we need to provide a center point, though radius is optional.
+    if(!lat || !lon) {
+        console.log("no coordinates provided, using default");
+        lat = TRP.currLoc.lat;
+        lon = TRP.currLoc.lon;
+    }
+
+    function getResults() {
+        var endpoint = TRP.fsAPI + "/venues/explore";
+        var dateString = TRP.getDateString();
+        var requestUrl = endpoint + "?";
+        requestUrl    += "ll=" + lat + "," + lon;
+        if (search) {
+            requestUrl += "&query=" + search;
+        }
+        //we probably want to request as many as possible, then cache. 
+        requestUrl    += "&limit=50"; 
+        requestUrl    += "&oauth_token=" + TRP.oauth;
+        requestUrl    += "&v=" + dateString;
+        // console.log(requestUrl);
+        TRP.indicateLoad(requestUrl);
+        $.get(requestUrl,processData);
+    }
+    function processTips(rawResult) {
+        var imgThumb = false;
+        var currentTip;
+        var tips = [];
+        var result = {};
+        if(rawResult.tips) {
+            for (var i =0; i<rawResult.tips.length; i++ ) {
+                var thisTip = rawResult.tips[i];
+                if (thisTip.photourl && !imgThumb) {
+                    imgThumb = thisTip.photourl;
+                } //no else
+                var data = {
+                    text: thisTip.text,
+                    firstName: thisTip.user.firstName,
+                    lastName: thisTip.user.lastName,
+                    photoUrl: thisTip.photourl
+                }
+                currentTip = new TRP.Comment(data);
+                tips.push(currentTip);
+            }
+        }
+        result.tips = tips;
+        result.thumb = imgThumb;
+        //this will be unreachable if we have any thumbnail already.
+        return result;
+    }
+
+    function processLocation(location) {
+        var loc = { 
+            lat: location.lat,
+            lon: location.lng
+        };
+        var address = location.address;
+            address += "\n " + location.city;
+            address += ", " + location.state;
+            address += " " + location.postalCode;
+        var returnObj = {
+            coord    : loc,
+            'address': address
+        };
+        return returnObj;
+    }
+
+    function processData(data) {
+        // console.log(data);
+        var processedData = [];
+        var statusCode = data.meta.code;
+        var resultCount = data.response.totalResults;
+        var rawResults = data.response.groups[0].items;
+
+        // console.log(statusCode);
+        // console.log(resultCount);
+        // console.log(rawResults);
+
+        for (var i=0; i<rawResults.length; i++) {
+            var thisResult = rawResults[i];
+            var feedback = processTips(thisResult);
+            var name = thisResult.venue.name;
+            var rating = thisResult.venue.rating;
+            var url = thisResult.venue.url;
+            var location = processLocation(thisResult.venue.location);
+            var tips = feedback.tips;
+            var thumb = feedback.thumb;
+
+            var venueData = {
+                'name': name,
+                'rating': rating,
+                'url': url,
+                'thumbnail' : thumb,
+                'locInfo': location,
+                'comments': tips
+            }
+
+            processedData.push(new TRP.Venue(venueData));
+        }
+        //designed to be used asynchronously
+        //calls callback passed to getResults.
+        callback(processedData);
+    }
+
+    getResults();
+}
+
+
+TRP.getSearchResults = function(lat,lon,search,callback) {
+
+    function getResults(search,lat,lon) {
+        var endpoint = TRP.fsAPI + "/venues/search";
+        dateString = TRP.getDateString();
+    }
+}
+
+TRP.indicateLoad = function(url) {
+    //this is a function that can be used for more complex ui info
+    //for now just console log "loading"
+    console.log("Loading... (" + url + ")");
+}
+
+//begin application
+var searchObj = {
+    callback: function(data) {
+        console.log(data);
+    },
+    search: "bars"
+};
+TRP.getSuggestions(searchObj);
+// delete searchObj.search;
+
+// TRP.getSuggestions(searchObj);
+searchObj.search = "museums";
+TRP.getSuggestions(searchObj);
